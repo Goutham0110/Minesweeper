f20472bb6d100c70696d3894336b76560d9e3b2c
Goutham <gouthamsaravananr@gmail.com>
2024-11-04 21:47:35 +0530

    prettified

--- src/components/game-info-panel.js
+++ src/components/game-info-panel.js
@@ -8,12 +8,12 @@ export default function GameInfoPanel() {
     if (gameState.isBoom) gameStatus = "💥💥💥Boom💣 Game Over💥💥💥";
     if (!gameState.unexploredNonBombBlocksCount) gameStatus = "You have found all the mines💣  Good Game🥳";
 
-    if(!gameStatus) gameStatus = `Keep Going! Still ${gameState.unexploredNonBombBlocksCount} blocks to explore 🔍`;
+    if (!gameStatus) gameStatus = `Keep Going! Still ${gameState.unexploredNonBombBlocksCount} blocks to explore 🔍`;
 
     return (
         <div className="gameInfoPanel">
             <h3>💣   M - I - N - E - S - W - E - E - P - E - R   💣</h3>
-            <h5>{gameStatus }</h5>
+            <h5>{gameStatus}</h5>
         </div>
     )
 }
